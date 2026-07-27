445af5ad257aea4655afdca4499cc139d0bda2e2
yatsuraivan29-droid <yatsuraivan29@gmail.com>
2026-07-27 11:14:00 +0000

    start landing

---
+++ js/data.js
@@ -0,0 +1,27 @@
+export const data = {
+  company: {
+    name: "",
+    slogan: "",
+    description: "",
+    phone: "",
+    email: "",
+    address: ""
+  },
+
+  hero: {
+    title: "",
+    subtitle: "",
+    button: ""
+  },
+
+  services: [],
+
+  gallery: [],
+
+  reviews: [],
+
+  social: {
+    facebook: "",
+    instagram: ""
+  }
+};
