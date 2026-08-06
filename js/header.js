660b7cd2c8ebdfcfd0bca58ac63bbc40a3fc0d17
yatsuraivan29-droid <yatsuraivan29@gmail.com>
2026-08-06 15:31:58 +0000

    burger

--- js/header.js
+++ js/header.js
@@ -3,8 +3,13 @@ export function header(data) {
     <header class="header">
         <div class="container">
             <div class="header__inner">
+                <div id = "burger" class="burger-menu">
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </div>
                 <a href="#" class="header__logo">${data.company.logo}</a>
-                <nav class="header__menu">
+                <nav id = "hederMenu" class="header__menu">
                     ${data.header.navigation.map(item => `<a href="${item.href}" class="header__link">${item.title}</a>`).join('')}
                 </nav>
                 <a href="${data.company.phone}" class="button header__button">${data.header.button.title}</a>
--- main.js
+++ main.js
@@ -12,6 +12,8 @@ import { data } from "./js/data.js";
 // ===== DOM =====
 const app = document.getElementById('app');
 
+
+
 // ===== Рендер =====
 function render(){
     app.innerHTML =`
@@ -27,3 +29,23 @@ function render(){
 };
 // ===== Запуск =====
 render()
+openMenu()
+
+function openMenu(){
+const burger = document.getElementById('burger');
+const hederMenu = document.getElementById('hederMenu');
+let burgerLisen = false
+
+burger.addEventListener("click",() => {
+    if (!burgerLisen){
+        burgerLisen = !burgerLisen
+        burger.classList.add('burger-menu-open')
+        hederMenu.classList.add('header__menu-open')
+    } else {
+        burgerLisen = !burgerLisen
+        burger.classList.remove('burger-menu-open')
+    hederMenu.classList.remove('header__menu-open')
+    }
+    
+})
+}
